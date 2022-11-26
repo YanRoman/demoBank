82c74a02dfc34e647fd8f36cdce1bbfae1a62ec2
Yan <romanets2003@bk.ru>
2022-11-26 17:31:06 +0300

    Доработка Аутентификации

--- src/main/java/com/demoBank/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.demoBank.domain;
-
-public enum Role {
-    USER;
-}
--- src/main/java/com/demoBank/repos/UserBankRepo.java
+++
@@ -1,8 +0,0 @@
-package com.demoBank.repos;
-
-import com.demoBank.domain.UserBank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBankRepo extends JpaRepository<UserBank, Long> {
-    UserBank findByUsername(String username);
-}
